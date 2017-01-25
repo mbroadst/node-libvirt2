7a07aabeca5f519b53f3894fef66cc0a121b660a
Matt Broadstone <mbroadst@gmail.com>
2017-01-25 14:03:13 -0500

    refactor(lookup): move lookupBy* methods to `Connect` class

--- test/test.js
+++ test/test.js
@@ -1,5 +1,27 @@
 'use strict';
-const proto = require('../lib/protocol').remote_protocol;
+const Connection = require('../lib/connection');
 
-console.dir(proto, { depth: null });
+let client = new Connection();
+client.connect({ host: '192.168.1.11', port: 16509 }, err => {
+  if (err) {
+    console.log('error: ', err);
+    process.exit(1);
+    return;
+  }
+
+  console.log('connected!');
+
+  // client.getHostname((err, result) => {
+  //   console.log('bananas');
+  //   console.log('result: ', result);
+  // });
+
+  client.getHostname()
+    .then(hostname => console.log('hostname: ', hostname))
+    .catch(err => console.log(err));
+
+  client.getSysinfo(0)
+    .then(sysinfo => console.log('sysinfo: ', sysinfo))
+    .catch(err => console.log(err));
+});
 
