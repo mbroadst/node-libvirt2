70feaeece64202a1796038eec2730ba99ae50f12
Matt Broadstone <mbroadst@gmail.com>
2017-01-27 12:46:45 -0500

    refactor(single-values): extract single values on method returns
    Where possible, we now extract single properties from method return
    objects that only contain a single property.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 'use strict';
-const Connection = require('../lib/connection');
+const Promise = require('bluebird'),
+      Connection = require('../lib/connection');
 
 let client = new Connection();
-client.connect({ host: '192.168.11.77', port: 16509 }, err => {
+client.connect({ host: '127.0.0.1', port: 16509 }, err => {
   if (err) {
     console.log('error: ', err);
     process.exit(1);
@@ -10,18 +11,10 @@ client.connect({ host: '192.168.11.77', port: 16509 }, err => {
   }
 
   console.log('connected!');
-
-  // client.getHostname((err, result) => {
-  //   console.log('bananas');
-  //   console.log('result: ', result);
-  // });
-
-  client.getHostname()
-    .then(hostname => console.log('hostname: ', hostname))
-    .catch(err => console.log(err));
-
-  client.getSysinfo(0)
-    .then(sysinfo => console.log('sysinfo: ', sysinfo))
+  client.listDomains(100)
+    .then(domains => Promise.map(domains, id => client.lookupDomainById(id)))
+    .then(domains => Promise.map(domains, d => d.getState(0)))
+    .then(names => console.log('state: ', names))
     .catch(err => console.log(err));
 });
 
